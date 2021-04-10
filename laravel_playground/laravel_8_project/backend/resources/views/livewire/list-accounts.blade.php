fad4a5e2ef1795b18dd64c9f08057452799d405d
Ryota Bannai <ryotala0528@gmail.com>
2021-04-10 22:51:15 +0900

    add: layouted with tailwind

--- laravel_playground/laravel_8_project/backend/resources/views/livewire/list-accounts.blade.php
+++ laravel_playground/laravel_8_project/backend/resources/views/livewire/list-accounts.blade.php
@@ -5,39 +5,40 @@
 </x-slot>
 
 <div>
-    The first livewire.
-    @foreach($accounts as $account)
-        {{ $account->name }}
-        <br>
-    @endforeach
-    <form wire:submit.prevent="save">
-        <div>
-            @if (session()->has('message'))
-                <div class="alert alert-success">
-                    {{ session('message') }}
-                </div>
-            @endif
-        </div>
-        @if (session()->has('sweet_alert.alert'))
-        <script>
-            // swal("{!! session()->get('sweet_alert.alert') !!}");
-            toastr.warning("{!! session()->get('sweet_alert.alert') !!}");
-        </script>
-        @endif
-
-        <input wire:model="name" type="text">
-        <br>
-        {{-- @error('name') 
-            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <strong class="font-bold">Holy smokes!</strong>
-                <span class="block sm:inline">{{ $message }}</span>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        The first livewire.
+        @foreach($accounts as $account)
+            {{ $account->name }}
+            <br>
+        @endforeach
+        <form wire:submit.prevent="save">
+            <div>
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
             </div>
-        @enderror --}}
-        <x-jet-input-error for="name"></x-jet-input-error>
-        <br>
-        <x-jet-action-message class="ml-3" on="saved">Done</x-jet-action-message>
-        <x-jet-button wire:loading.attr="disabled">Save</x-jet-button>
-    </form>
-    <button wire:click="$emitUp('banner-message')">Reset</button>
+            @if (session()->has('sweet_alert.alert'))
+            <script>
+                // swal("{!! session()->get('sweet_alert.alert') !!}");
+                toastr.warning("{!! session()->get('sweet_alert.alert') !!}");
+            </script>
+            @endif
 
+            <input wire:model="name" type="text">
+            <br>
+            {{-- @error('name') 
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">Holy smokes!</strong>
+                    <span class="block sm:inline">{{ $message }}</span>
+                </div>
+            @enderror --}}
+            <x-jet-input-error for="name"></x-jet-input-error>
+            <br>
+            <x-jet-action-message class="ml-3" on="saved">Done</x-jet-action-message>
+            <x-jet-button wire:loading.attr="disabled">Save</x-jet-button>
+        </form>
+        <button wire:click="$emitUp('banner-message')">Reset</button>
+    </div>
 </div>
