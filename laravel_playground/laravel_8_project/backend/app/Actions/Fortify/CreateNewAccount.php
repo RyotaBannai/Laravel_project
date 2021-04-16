b9975af7875d4c5c333fe0a089683a788cf1ab4e
Ryota Bannai <ryotala0528@gmail.com>
2021-04-16 23:05:37 +0900

    add: checked html form can has colon to send.

--- laravel_playground/laravel_8_project/backend/app/Actions/Fortify/CreateNewAccount.php
+++ laravel_playground/laravel_8_project/backend/app/Actions/Fortify/CreateNewAccount.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Fortify;
 
 use App\Models\Account;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -18,8 +19,15 @@ class CreateNewAccount implements CreatesNewUsers
      * @param  array  $input
      * @return \App\Models\Account
      */
-    public function create(array $input)
+    public function create(array $inputs)
     {
+        foreach($inputs as $name => $value){
+            if(Str::contains($name, ':')){
+                [$ops, $column] = explode(':', $name);
+                // dd($ops, $column);
+            }
+        }
+
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:accounts'],
